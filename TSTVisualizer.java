a495d1dc26d236b30aec6afc7fabe9cf03222c81
YlldritQ_03 <yq68447@ubt-uni.net>
2024-07-01 07:34:15 +0200

    Final

--- TSTVisualizer.java
+++ TSTVisualizer.java
@@ -2,14 +2,8 @@
 public class TSTVisualizer {
     // Kode ANSI për ngjyrosjen e tekstit në konzolë
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
 
     // Metodë për vizualizimin e TST-së duke përdorur tekst të ngjyrosur dhe indentim
     public static void visualizeTST(AutocompleteSystem.TSTNode node, String indent, boolean last) {
